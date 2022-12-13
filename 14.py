ea8602a3a8865c5e43a2b89980b5c89da5f458f4
Jacob Mischka <jacob@mischka.me>
2022-12-13 06:48:45 -0600

    Add day 14 part 2

--- 14.py
+++ 14.py
@@ -17,9 +17,20 @@ def main():
 
     print("Part 1:", index - 1)
 
+    keys: list[tuple[int, str]] = []
+    index = 0
+    while len(keys) < 64:
+        key = test_key(index, 2016)
+        if key is not None:
+            keys.append((index, key))
 
-def test_key(index: int) -> str | None:
-    hash = get_hash(index)
+        index += 1
+
+    print("Part 2:", index - 1)
+
+
+def test_key(index: int, stretch_value: int = 0) -> str | None:
+    hash = get_hash(index, stretch_value)
     repeat = None
     for i in range(len(hash) - 2):
         if hash[i] == hash[i + 1] and hash[i] == hash[i + 2]:
@@ -31,7 +42,7 @@ def test_key(index: int) -> str | None:
 
     for j in range(1000):
         new_index = index + j + 1
-        new_hash = get_hash(new_index)
+        new_hash = get_hash(new_index, stretch_value)
         s = "".join(repeat for _ in range(5))
         if s in new_hash:
             return hash
@@ -39,15 +50,21 @@ def test_key(index: int) -> str | None:
     return None
 
 
-hash_cache: dict[int, str] = {}
+hash_cache: dict[int, dict[int, str]] = {}
 
 
-def get_hash(index: int) -> str:
+def get_hash(index: int, stretch_value: int = 0) -> str:
     try:
-        return hash_cache[index]
+        return hash_cache[stretch_value][index]
     except KeyError:
         hash = md5(f"{salt}{index}".encode("utf-8")).hexdigest()
-        hash_cache[index] = hash
+        for _ in range(stretch_value):
+            hash = md5(hash.encode("utf-8")).hexdigest()
+
+        if stretch_value not in hash_cache:
+            hash_cache[stretch_value] = {}
+
+        hash_cache[stretch_value][index] = hash
         return hash
 
 
